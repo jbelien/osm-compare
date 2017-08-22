931663d244e80f362c9677c6366bb2fe6d215426
Jonathan Beliën <dev@jbelien.be>
2017-08-22 22:33:49 +0200

    Basic GeoJSON upload from a URL
    Still need some work !

--- routes/upload.js
+++ routes/upload.js
@@ -6,6 +6,7 @@ var proj4 = require("proj4");
 var fetch = require("node-fetch");
 var debug = require("debug")("osm-compare:upload");
 var geojsonhint = require("@mapbox/geojsonhint");
+var validurl = require("valid-url");
 
 var router = express.Router();
 var upload = multer({ dest: "uploads/" })
@@ -69,6 +70,50 @@ router.post("/", upload.single("file"), function(req, res, next) {
       });
     }
 */
+  } else if (typeof req.body.url !== "undefined" && validurl.isUri(req.body.url)) {
+    fetch(req.body.url).then(function(response) {
+      return response.text();
+    }).then(function(text) {
+      try {
+        var geojson = JSON.parse(text);
+        var errors = geojsonhint.hint(geojson);
+
+        if (errors.length > 0) {
+          throw new Exception(errors);
+        } else {
+          fs.writeFile("uploads/test.geojson", text, function(error) {
+            if (error) {
+              throw new Exception(error);
+            }
+            var envelope = turf.envelope(geojson);
+            var buffer = turf.buffer(envelope, 500, "meters", 1);
+            var area = turf.envelope(buffer);
+
+            req.session.file = {
+              path: "uploads/test.geojson",
+              name: "test" || null
+            };
+
+            res.render("upload", {
+              area: area,
+              file: "test" || null,
+              geojson: geojson
+            });
+          });
+        }
+      } catch (exception) {
+        res.render("upload", {
+          errors: [exception],
+          url: req.body.url
+        });
+      }
+    }).catch(function(error) {
+      debug(error.message);
+      res.render("upload", {
+        errors: [error],
+        url: req.body.url
+      });
+    });
   } else {
     res.render("upload", {});
   }
