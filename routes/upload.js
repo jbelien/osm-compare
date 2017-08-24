0f65d63f4e7489dac9e86f9da3bbf895823f9986
Jonathan Beliën <dev@jbelien.be>
2017-08-24 21:42:09 +0200

    Improve GeoJSON upload process

--- routes/upload.js
+++ routes/upload.js
@@ -1,122 +1,89 @@
 var express = require("express");
+var debug = require("debug")("osm-compare:upload");
 
 var crypto = require('crypto');
-var multer = require("multer");
-var fs = require("fs");
-var turf = require("@turf/turf");
-var proj4 = require("proj4");
 var fetch = require("node-fetch");
-var debug = require("debug")("osm-compare:upload");
+var fs = require("fs");
 var geojsonhint = require("@mapbox/geojsonhint");
-var validurl = require("valid-url");
-var url = require("url");
+var multer = require("multer");
 var path = require("path");
+var proj4 = require("proj4");
+var turf = require("@turf/turf");
+var url = require("url");
+var validurl = require("valid-url");
 
 var router = express.Router();
 var upload = multer({ dest: "uploads/" })
 
-router.get("/", function(req, res, next) {
-  delete req.session.file;
-  delete req.session.overpass;
-
-  res.render("upload", {});
-});
-
-router.post("/", upload.single("file"), function(req, res, next) {
-  if (typeof req.file !== "undefined") {
-    var geojson = JSON.parse(fs.readFileSync(req.file.path, "utf8"));
-
+function processFile(req, res, file, filename, url) {
+  try {
+    var text = fs.readFileSync(file, "utf8");
+    var geojson = JSON.parse(text);
     var errors = geojsonhint.hint(geojson);
 
     if (errors.length > 0) {
-      res.render("upload", {
-        errors: errors,
-        file: req.file.originalname || null
-      });
+      throw errors;
     } else {
       var envelope = turf.envelope(geojson);
       var buffer = turf.buffer(envelope, 500, "meters", 1);
       var area = turf.envelope(buffer);
 
       req.session.file = {
-        path: req.file.path,
-        name: req.file.originalname || null
+        path: file,
+        name: filename
       };
 
       res.render("upload", {
         area: area,
-        file: req.file.originalname || null,
+        file: filename,
         geojson: geojson
       });
     }
-/*
-    var crs = "urn:ogc:def:crs:EPSG:4326";
-    var epsg = "4326";
-    if (typeof geojson.crs !== "undefined") {
-      if (geojson.crs.type === "name") {
-        crs = geojson.crs.properties.name;
-        epsg = /^(?:urn\:ogc\:def\:crs\:)?EPSG\:(.+)$/.exec(crs);
-        epsg = parseInt(/([0-9]+)/.exec(epsg));
-      } else if (geojson.crs.type === "EPSG") {
-        epsg = geojson.crs.properties.code;
-      } else {
-        throw new Error("CRS defined in crs section could not be identified : " + JSON.stringify(geojson.crs));
-      }
+  } catch (e) {
+    res.render("upload", {
+      errors: (typeof e !== "array" ? [e] : e),
+      file: filename,
+      url: url
+    });
+  }
+}
 
-      fetch("https://epsg.io/" + epsg + ".proj4").then(function(response) {
-        debug("Fetch: https://epsg.io/" + epsg + ".proj4");
+router.get("/", function (req, res, next) {
+  delete req.session.file;
+  delete req.session.overpass;
 
-        return response.text();
-      }).then(function(text) {
-        debug("EPSG:" + epsg + " = \"" + text + "\"");
+  res.render("upload", {});
+});
 
-        proj4.defs("EPSG:" + epsg, text);
-      });
-    }
-*/
+router.post("/", upload.single("file"), function (req, res, next) {
+  if (typeof req.file !== "undefined") {
+    var file = req.file.path;
+    var filename = req.file.originalname || null;
+
+    processFile(req, res, file, filename);
   } else if (typeof req.body.url !== "undefined" && validurl.isUri(req.body.url)) {
-    fetch(req.body.url).then(function(response) {
+    var file = "uploads/" + crypto.randomBytes(16).toString("hex");
+    var filename = path.basename(url.parse(req.body.url).pathname) || "no-name";
+
+    debug(filename);
+    debug(req.body.url);
+
+    fetch(req.body.url).then(function (response) {
       return response.text();
-    }).then(function(text) {
-      try {
-        var geojson = JSON.parse(text);
-        var errors = geojsonhint.hint(geojson);
-
-        if (errors.length > 0) {
-          throw new Exception(errors);
-        } else {
-          var temp = "uploads/" + crypto.randomBytes(16).toString('hex');
-
-          fs.writeFile(temp, text, function(error) {
-            if (error) {
-              throw new Exception(error);
-            }
-            var envelope = turf.envelope(geojson);
-            var buffer = turf.buffer(envelope, 500, "meters", 1);
-            var area = turf.envelope(buffer);
-
-            req.session.file = {
-              path: temp,
-              name: path.basename(url.parse(req.body.url).pathname)
-            };
-
-            res.render("upload", {
-              area: area,
-              file: path.basename(url.parse(req.body.url).pathname),
-              geojson: geojson
-            });
-          });
+    }).then(function (text) {
+      fs.writeFile(file, text, function (error) {
+        if (error) {
+          throw error;
         }
-      } catch (exception) {
-        res.render("upload", {
-          errors: [exception],
-          url: req.body.url
-        });
-      }
-    }).catch(function(error) {
+
+        processFile(req, res, file, filename, req.body.url);
+      });
+    }).catch(function (error) {
       debug(error.message);
+
       res.render("upload", {
         errors: [error],
+        file: filename,
         url: req.body.url
       });
     });
